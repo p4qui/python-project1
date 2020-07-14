8f31f6f4b8877da760d1deb65712d1a127c815e8
p4qui <68265550+p4qui@users.noreply.github.com>
2020-07-13 22:26:41 -0700

    Create latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,29 @@
+import os
+import sys
+os.system('clear')
+os.system('figlet Syafaq')
+author = "Muchammad Abdillah Syafaq"
+youtube = "Y.C.Syafaq"
+Instagram = "@abfak__"
+print("-----------------------------------------")
+print("Author " + ": " + author)
+print("YouTube " + ": " + youtube)
+print("Instagram " + ": " + Instagram)
+print("-----------------------------------------")
+nama = raw_input("masukkan nama : ")
+print("halo " + nama + "!" + "kita mulai ya gamenya!")
+hobi1 = "Hacking"
+hobi2 = "Penetration Testing"
+hobi3 = "Programming"
+faq1 = "Hacker Yang Handal"
+faq2 = "Pentester Yang Handal"
+faq3 = "Programmer Yang Handal"
+print("[?]Pilih Hobi Kamu")
+print("1. " + hobi1)
+print("2. " + hobi2)
+print("3. " + hobi3)
+pilih = raw_input("[?]Nomer: ")
+if pilih == "1":
+        print("wah keren ya " + nama + "!" + "hobi kamu " + hobi1 + "!" + "semoga kamu menjadi " + faq1)
+if pilih == "2":                                                                                                                                                                print("wah keren ya " + nama + "!" + "hobi kamu " + hobi2 + "!" + "semoga kamu menjadi " + faq2)                                                                if pilih == "3":
+        print("wah keren ya " + nama + "!" + "hobi kamu " + hobi3 + "!" + "semoga kamu menjadi " + faq3)
